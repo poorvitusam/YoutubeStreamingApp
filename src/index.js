9d741b369a7716dc8f89f0c72b4d9643dc28b7b8
poorvitusam <poorvitusam@gmail.com>
2018-04-14 21:01:20 -0400

    Project Skeleton

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
 import reducers from './reducers';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
+const API_KEY = 'AIzaSyBNBsABd3oSuDkW__bIOGwUGWd9fvkRBzY';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({videos});
+		});
+	}
+	render() {
+		return (
+		  <div>
+		  	<SearchBar/>
+		  </div>
+		);
+	}
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
