3b32e3cbacb07985b6e2d02a5c977e7292b6ba30
poorvitusam <poorvitusam@gmail.com>
2018-04-16 01:14:59 -0400

    Code refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import reducers from './reducers';
-
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBNBsABd3oSuDkW__bIOGwUGWd9fvkRBzY';
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 class App extends Component {
 	constructor(props) {
@@ -48,7 +42,5 @@ class App extends Component {
 }
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
   , document.querySelector('.container'));
