c0b1e85b3330f14a27ffd041af72dc4b9c41e912
poorvitusam <poorvitusam@gmail.com>
2018-04-15 21:01:03 -0400

    Search bar implemented

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,9 +12,14 @@ class SearchBar extends Component {
 			<div>
 				<input 
 					value = {this.state.term}
-					onChange={event => this.setState({term:event.target.value})}/>
+					onChange={event => this.onInputChange(event.target.value)}/>
 			</div>
 		);	
+	}
+
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchChange(term);
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -17,18 +17,29 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
-
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({videos});
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('surfboards');
+		
+	}
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 	render() {
 		return (
 		  <div>
-		  	<SearchBar/>
-		  	<VideoDetail video={this.state.videos[0]}/>
-		  	<VideoList videos={this.state.videos}/>
+		  	<SearchBar onSearchChange={event => this.videoSearch(event)}/>
+		  	<VideoDetail video={this.state.selectedVideo}/>
+		  	<VideoList 
+		  		onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+		  		videos={this.state.videos}/>
 		  </div>
 		);
 	}
