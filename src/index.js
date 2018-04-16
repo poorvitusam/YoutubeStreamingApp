fe24fd94243d123c95f24cba33d5bb8f77cc2052
poorvitusam <poorvitusam@gmail.com>
2018-04-15 21:15:31 -0400

    Load time added after every search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -33,9 +34,10 @@ class App extends Component {
 		});
 	}
 	render() {
+		const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
 		return (
 		  <div>
-		  	<SearchBar onSearchChange={event => this.videoSearch(event)}/>
+		  	<SearchBar onSearchChange={event => videoSearch(event)}/>
 		  	<VideoDetail video={this.state.selectedVideo}/>
 		  	<VideoList 
 		  		onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
